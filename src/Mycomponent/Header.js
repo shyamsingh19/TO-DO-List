26ff15fc1112d8fec289008f7aa821e873522cbe
shyam sunder singh <shyamsunders507@gmail.com>
2024-03-11 23:48:08 +0530

    dropdown-toggle removed

--- src/Mycomponent/Header.js
+++ src/Mycomponent/Header.js
@@ -36,15 +36,6 @@ export default class header extends PureComponent {
                   </a>
                 </li>
                 <li className="nav-item dropdown">
-                  {/* <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a> */}
-                  {/* <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><hr className="dropdown-divider"/></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul> */}
                 </li>
                 <li className="nav-item"></li>
               </ul>
